13306f26af304640aeb1ddba4365162fb33b2e69
Jose <jgenao588@gmail.com>
2024-03-08 18:38:36 -0400

    infra IoC implemented

--- Bank_Users/Program.cs
+++ Bank_Users/Program.cs
@@ -1,11 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using Users.Infra.Context;
+using Users.Infra;
+
 var builder = WebApplication.CreateBuilder(args);
-
 // Add services to the container.
-
 builder.Services.AddControllers();
+builder.Services.AddDbContext<UsersDbContext>(opt =>
+{
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services
+    .AddRepositories();
 
 var app = builder.Build();
 
--- Users.App/IoC.cs
+++ Users.App/IoC.cs
@@ -1,7 +1,15 @@
-﻿namespace Users.App
-{
-    public class IoC
-    {
+﻿using Microsoft.Extensions.DependencyInjection;
+using Users.App.Interfaces;
+using Users.App.Services;
 
+namespace Users.App
+{
+    public static class IoC
+    {
+        public static IServiceCollection AddServices(this IServiceCollection services)
+        {
+            return services
+                .AddScoped<IUserServices, UsersService>();
+        }
     }
 }
